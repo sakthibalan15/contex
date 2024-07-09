5f3cde8a154044d27d628a41004a786f1af143e8
sangeetha <sangeethailango21@gmail.com>
2024-07-09 15:42:46 +0530

    Change font family of labels

--- lib/chart/plot.ex
+++ lib/chart/plot.ex
@@ -62,7 +62,9 @@ defmodule Contex.Plot do
   @x_axis_tick_labels 70
   @default_style """
   <style type="text/css"><![CDATA[
+    @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');
     text {fill: black}
+    text {font-family: 'Lato', sans-serif}
     line {stroke: #fff}
   ]]></style>
   """
