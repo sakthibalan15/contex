489fbcfc59277bae202c9caab4b5c259906178ed
sangeetha <sangeethailango21@gmail.com>
2024-07-12 15:02:28 +0530

    Change color of labels

--- lib/chart/plot.ex
+++ lib/chart/plot.ex
@@ -63,7 +63,7 @@ defmodule Contex.Plot do
   @default_style """
   <style type="text/css"><![CDATA[
     @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');
-    text {fill: black}
+    text {fill: rgba(74,74,74,255)}
     text {font-family: 'Lato', sans-serif}
     .normal_stroke line {stroke: #ffdfae}
     .white_stroke line {stroke: #fff}
